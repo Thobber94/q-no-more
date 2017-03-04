7b471d40bef954543172d4332c4d2209f3b8569b
Thobber94 <thomas.bergby@gmail.com>
2017-03-04 04:44:50 +0100

    User info gets collected
    Everything looks good!

--- QnoMore/app/src/main/java/net/bergby/qnomore/MainActivity.java
+++ QnoMore/app/src/main/java/net/bergby/qnomore/MainActivity.java
@@ -2,45 +2,47 @@ package net.bergby.qnomore;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.*;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.squareup.picasso.Picasso;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener
 {
-
-
     // Global variables
     // Set the Google API client
     private GoogleApiClient mGoogleApiClient;
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
-    //private NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-    //private View hView = navigationView.getHeaderView(0);
-    //TextView nav_user = (TextView) hView.findViewById(R.id.nav_view_user);
-    //TextView nav_email = (TextView) hView.findViewById(R.id.nav_view_email);
     private ProgressDialog mProgressDialog;
 
+    private TextView nav_user;
+    private TextView nav_email;
+    private CircleImageView nav_userImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +50,66 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View hView = navigationView.getHeaderView(0);
+        nav_user = (TextView) hView.findViewById(R.id.nav_view_user);
+        nav_email = (TextView) hView.findViewById(R.id.nav_view_email);
+        nav_userImage = (CircleImageView) hView.findViewById(R.id.profile_image);
+
         //Button listener
         findViewById(R.id.sign_in_button).setOnClickListener(this);
 
+        // Google SignIn API
+        googleAuthenticator();
+    }
+
+    // Check if cached login is valid or not
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone())
+        {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        }else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    private void googleAuthenticator()
+    {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -61,11 +120,12 @@ public class MainActivity extends AppCompatActivity
 
         // Build a GoogleApiClient with access to the Google Sign-In API and the
         // options specified by gso.
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
     }
+
     private void initSidebar()
     {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -87,7 +147,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        //setNavHeaderInfo();
     }
 
     @Override
@@ -143,13 +202,14 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void setNavHeaderInfo()
+    private void setNavHeaderInfo(String name, String email, Uri imageUrl)
     {
-
-        //nav_email.setText("thomas.bergby@gmail.com");
-        //nav_user.setText("Thomas Bergby");
+        nav_user.setText(name);
+        nav_email.setText(email);
+        Picasso.with(this)
+                .load(imageUrl)
+                .into(nav_userImage);
     }
-
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
@@ -194,7 +254,13 @@ public class MainActivity extends AppCompatActivity
         {
             // Signed in successfully
             GoogleSignInAccount userAccount = result.getSignInAccount();
-            System.out.println(userAccount.getDisplayName());
+            if (userAccount != null)
+            {
+                setNavHeaderInfo(userAccount.getDisplayName(), userAccount.getEmail(), userAccount.getPhotoUrl());
+                initSidebar();
+                findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+                findViewById(R.id.tvLogin).setVisibility(View.GONE);
+            }
         }
         else
         {
