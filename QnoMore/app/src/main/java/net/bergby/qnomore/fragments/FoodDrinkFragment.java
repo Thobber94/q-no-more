06eedfa60b62d8d54a6490d8b236ea26967fe620
Thobber94 <thomas.bergby@gmail.com>
2017-03-13 20:16:30 +0100

    Added images and the fragment

---
+++ QnoMore/app/src/main/java/net/bergby/qnomore/fragments/FoodDrinkFragment.java
@@ -0,0 +1,29 @@
+package net.bergby.qnomore.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.bergby.qnomore.R;
+
+
+public class FoodDrinkFragment extends Fragment
+{
+
+    public FoodDrinkFragment()
+    {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food_drink, container, false);
+    }
+
+}
